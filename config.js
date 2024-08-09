9586b30948de2386293b85eaa66195ba17b80a00
A-m-u-r <112509251+A-m-u-r@users.noreply.github.com>
2024-08-09 16:38:36 +0500

    Другой бот

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    token: '7384558323:AAG773v00DYQru7uJLpW-a-q5e5gxzZJ_og',
+    token: '7085722719:AAFTn4XZX_erwtiAGO1fdVbD2hiMpodkOUM',
     superAdminId: "562533452"
 };
 
 
 /*
-'7085722719:AAFTn4XZX_erwtiAGO1fdVbD2hiMpodkOUM',*/
+'7085722719:AAFTn4XZX_erwtiAGO1fdVbD2hiMpodkOUM',
+
+
+'7384558323:AAG773v00DYQru7uJLpW-a-q5e5gxzZJ_og'*/
