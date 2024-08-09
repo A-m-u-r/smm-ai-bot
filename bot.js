8a16cb01dcb3e545e84af9ef4b916039e39bc636
A-m-u-r <112509251+A-m-u-r@users.noreply.github.com>
2024-08-09 16:27:52 +0500

    Добавил возможность отправлять фото

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 const TelegramApi = require("node-telegram-bot-api");
 const {token, superAdminId} = require('./config');
-const {askClaude} = require('./claudeApi');
+const {askClaude, askClaudeImage} = require('./claudeApi');
 const {isAdmin, isSuperAdmin, setRole, getRole} = require('./userRoles');
 const {activeContexts, userContexts} = require("./contextManager");
 const contextManager = require('./contextManager');
-
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
 const {
     handleStart,
     handleInfo,
@@ -31,6 +33,7 @@ const userPrompts = {};
 const db = require('./database');
 const {addTokens} = require("./tokens");
 const {getBalance, spendTokens, addUserWithInitialBalance} = require("./database");
+
 db.init().then(() => {
     console.log('Database initialized');
 
@@ -95,6 +98,11 @@ function createSizeKeyboard() {
     };
 }
 
+const downloadFile = async (fileLink, filePath) => {
+    const response = await get(fileLink, {responseType: 'arraybuffer'});
+    fs.writeFileSync(filePath, Buffer.from(response.data, 'binary'));
+    return Buffer.from(response.data, 'binary');
+};
 const start = () => {
     bot.on('callback_query', async (query) => {
         const chatId = query.message.chat.id;
@@ -135,11 +143,63 @@ const start = () => {
         }
 
     });
+    bot.on('photo', async (msg) => {
+        const chatId = msg.chat.id;
+        const fileId = msg.photo[msg.photo.length - 1].file_id;
+
+        // Получаем файл изображения
+        const fileLink = await bot.getFileLink(fileId);
+        const imagePath = path.join(__dirname, 'received_image.jpg');
+        const imageBuffer = await downloadFile(fileLink, imagePath);
+
+        // Преобразуем буфер изображения в base64
+        const imageData = imageBuffer.toString('base64');
+
+        try {
+            // Отправляем запрос к прокси-серверу
+            const response = await axios.post('http://localhost:3001/api/anthropic', {
+                model: 'claude-3-5-sonnet-20240620',
+                max_tokens: 1024,
+                messages: [
+                    {
+                        "role": 'user',
+                        "content": [
+                            {
+                                "type": 'image',
+                                "source": {
+                                    "type": 'base64',
+                                    "media_type": 'image/jpeg',
+                                    "data": imageData,
+                                },
+                            },
+                            {"type": "text", "text": "Что на этом изображении?"},
+                        ],
+                    },
+                ],
+            });
+
+            // Отправляем результат распознавания пользователю
+            await bot.sendMessage(chatId, response.data.content[0].text);
+        } catch (error) {
+            console.error('Error calling proxy server:', error);
+            await bot.sendMessage(chatId, 'Произошла ошибка при распознавании изображения');
+        }
+    });
+
+// Функция для загрузки файла по ссылке
+    const downloadFile = async (fileLink, filePath) => {
+        const response = await axios.get(fileLink, {responseType: 'arraybuffer'});
+        fs.writeFileSync(filePath, Buffer.from(response.data, 'binary'));
+        return Buffer.from(response.data, 'binary');
+    };
 
     bot.on('message', async msg => {
         const text = msg.text;
         const chatId = msg.chat.id;
         const userId = msg.from.id;
+        if (msg.photo) {
+            return;
+        }
         const comm = ['Установить контекст', 'Сохранить контекст', 'Список контекстов', 'Переключить контекст', 'Удалить контекст', 'Проверить контекст', 'Ask AI', 'Контекст', 'Генерировать идеи', 'Генерировать пост']
         if (waitingStates[chatId]) {
             if (text === '/cancel' || text === 'Отмена') {
@@ -164,7 +224,18 @@ const start = () => {
 
                 if (waitingStates[chatId] === 'waiting_for_prompt') {
                     waitingMessage = await bot.sendMessage(chatId, "Пожалуйста, подождите...");
+                    /* if (msg.photo) {
+                         const fileId = msg.photo[msg.photo.length - 1].file_id;
+                         const fileLink = await bot.getFileLink(fileId);
+                         const imagePath = path.join(__dirname, 'received_image.jpg');
+                         const imageBuffer = await downloadFile(fileLink, imagePath);
 
+                         // Запрос к Claude для распознавания изображения
+                         const response = await askClaudeImage('Распознайте и опишите это изображение:', imageBuffer);
+
+                         // Отправляем результат распознавания пользователю
+                         await bot.sendMessage(chatId, response.content[0].text);
+                     } else {*/
                     await bot.sendMessage(superAdminId, `Пользователь ${msg.from.username || msg.from.first_name} (ID: ${userId}) отправил промпт:\n\n${text}`);
 
                     let response = await askClaude(text);
@@ -173,6 +244,7 @@ const start = () => {
                         reply_markup: createMainKeyboard(isAdmin)
                     });
                 }
+
                 if (waitingStates[chatId] === 'waiting_for_context') {
                     await handleSaveContext(bot, chatId, userId, text);
 
@@ -189,8 +261,7 @@ const start = () => {
                 } else if (waitingStates[chatId] === 'waiting_for_get_tokens') {
                     let response = await getBalance(text);
                     await bot.sendMessage(chatId, `Токены "${response}"`);
-                }
-                else if (waitingStates[chatId] === 'waiting_for_context_save') {
+                } else if (waitingStates[chatId] === 'waiting_for_context_save') {
                     const [contextName, ...contextParts] = text.split(':');
                     const contextData = contextParts.join(':').trim();
                     console.log(contextName, contextData)
@@ -366,7 +437,7 @@ const start = () => {
                     case 'Генерировать идеи':
                         let waitingMessage = await bot.sendMessage(chatId, "Пожалуйста, подождите...");
                         response = await handleGenerateIdeas(bot, chatId, userId);
-                        if(waitingMessage){
+                        if (waitingMessage) {
                             await bot.deleteMessage(chatId, waitingMessage.message_id);
                         }
                         break;
@@ -399,9 +470,9 @@ const start = () => {
             }
 
             // Отправляем ответ и клавиатуру после каждого сообщения
-              await bot.sendMessage(chatId, response || "Выберите команду:", {
-                  reply_markup: createMainKeyboard(userId)
-              });
+            await bot.sendMessage(chatId, response || "Выберите команду:", {
+                reply_markup: createMainKeyboard(userId)
+            });
         }
     })
 }
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    token: '7085722719:AAFTn4XZX_erwtiAGO1fdVbD2hiMpodkOUM',
+    token: '7384558323:AAG773v00DYQru7uJLpW-a-q5e5gxzZJ_og',
     superAdminId: "562533452"
 };
+
+
+/*
+'7085722719:AAFTn4XZX_erwtiAGO1fdVbD2hiMpodkOUM',*/
